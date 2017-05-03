ad9a3919e0aa3a2a97c98b832fa97895fef87140
Denis-12 <denis_matveev_99@mail.ru>
2017-05-03 20:48:58 +0700

    Fix switch; Deleted excess code; Add in Functions.ccp function: Stat

--- QuizRunner/main.cpp
+++ QuizRunner/main.cpp
@@ -10,10 +10,10 @@ using namespace std;
 //=============================
 int main(){
 srand(time(NULL));
-int A[31]={},error=0,col=0;
+FILE *fl;
+int A[31]={},error=0,col=0,n=0;
 char ch,k0=true,k1=true;
 struct tm * timeinfo;
-char buf[100];
 setlocale(LC_ALL, "Russian");
 while (k0==true){
 system("cls");
@@ -23,37 +23,20 @@ printf("3. Статистика\n");
 printf("4. Выход из программы\n");
 cout<<"Ваш выбор: ";
 cin>>ch;
-	if(ch!='1' and ch!='2' and ch!='3' and ch!='4'){
-		while(ch!='1' and ch!='2' and ch!='3' and ch!='4'){
-			system("cls");
-			printf("1. Начать тестирование\n");
-			printf("2. Unit-Tests\n");
-			printf("3. Статистика\n");
-			printf("4. Выход из программы\n");
-			cout<<"Ваш выбор: ";
-			cin>>ch;
-		}
-	}
+
 switch(ch) {
 case '1':
+		k1=true;
 		system("cls");
 		while (k1==true){
 			system("cls");
 			printf("1. Математика\n");
 			printf("2. История\n");
-			printf("3. Выход из тестирования\n");
+			printf("3. Программирование\n");
+			printf("4. Выход из тестирования\n");
 			cout<<"Ваш выбор: ";
 			cin>>ch;
-				if(ch!='1' and ch!='2' and ch!='3'){
-					while(ch!='1' and ch!='2' and ch!='3'){
-						system("cls");
-						printf("1. Математика\n");
-						printf("2. История\n");
-						printf("3. Выход из тестирования\n");
-						cout<<"Ваш выбор: ";
-						cin>>ch;
-						}
-					}
+			
 		switch(ch) {
 				case '1':
 					system("cls");
@@ -62,14 +45,43 @@ case '1':
 						for(int i=0; i<31;i++){
 							A[i]=0;
 						}
-					matan(A,&error,&col);
+						n=0;
+					QTest(A,n);
 					break;
+					
 				case '2':
+					system("cls");
+					error=0;
+					col=0;
+						for(int i=0; i<31;i++){
+							A[i]=0;
+						}
+						n=1;
+					QTest(A,n);
 					break;
+					
 				case '3':
-					k1=false;
+					system("cls");
+					error=0;
+					col=0;
+						for(int i=0; i<31;i++){
+							A[i]=0;
+						}
+						n=2;
+					QTest(A,n);
 					break;
+					
+				case '4':
+					k1=false;
+					fl=fopen("Statistics.txt","a+");
+					fprintf(fl,"\n===========================================\n");
+					fclose(fl);
+					break;
+					
 				default:
+					system("cls");
+					printf("Твой выбор еще не сделан...\n");
+					system("pause");
 					break;
 			}
 		}
@@ -79,29 +91,6 @@ case '2':
    break;
    
 case '3':
-   FILE *fl;//наЧАЛО функции
-   time_t data;
-   struct tm * timeinfo;
-   char buf[100];
-   time(&data);
-   timeinfo = localtime(&data);
-   strftime(buf,100,"Data:%x Time:%I:%M%p. ",timeinfo);
-   fl=fopen("Statistics.txt","a+");
-	   if(fl==NULL){
-	   	printf("Файл не открыт.\n");
-	   	break;
-	   }
-		else{
-			printf("Файл открыт успешно.\n");
-		}
-		fputs(buf,fl);
-		fprintf(fl,"\nМатематика:: Правильных ответов:%d Неправильных ответов:%d \nВопросы в которых допущена ошибка:",(col-error),error);
-		for(int i=0;i<error;i++){
-			fprintf(fl," №:%d",A[i+1]);
-		}
-		fprintf(fl,"\n===========================================\n");
-		system("pause");
-		fclose(fl);// Конец функции
    break;
    
 case '4':
@@ -109,7 +98,9 @@ case '4':
    break;
    
 default:
+	system("cls");
 	printf("Твой выбор еще не сделан..\n");
+	system("pause");
  	 }
 	}
 system ("PAUSE");

8fd7d41f8865636058456a2e6e04f2dac281d4ee
Gangster <www.petr54@mail.ru>
2017-05-30 01:58:07 +0700

    Add make file; Add Locale Libs; Fix bug in code.

--- QuizRunner/Src/Functions.h
+++ QuizRunner/Src/Functions.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#define SFML_STATIC
+#include <SFML/Graphics.hpp> 
+#include <SFML/Window.hpp>
 #include <iostream>
 #include <ctime>
 #include <stdlib.h>
@@ -10,6 +13,6 @@
 #include <windows.h>
 
 int Stat(int *, int , int , int, int);
-int QTest(int *A, int n);
+int QTest(int *A, int n, sf::RenderWindow &);
 
 #endif
